2f6c23f545886d0c97a8b44b5f8dd4f37a3effbd
Wesley Ting <wesleyting3@gmail.com>
2021-09-25 18:22:20 -0700

    Home page near completion

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Add function for hamburger to show overlay
 const body = document.body;
 const btn = document.querySelector('.hamburger');
 
@@ -6,9 +7,9 @@ btn.addEventListener('click', function(){
 });
 
 
-
+// Masonry Grid
 let magicGrid = new MagicGrid({
-    container: '.container',
+    container: '.masonry-grid',
     animate: true,
     gutter: 30,
     static: true,
@@ -16,3 +17,13 @@ let magicGrid = new MagicGrid({
   });
   
   magicGrid.listen();
+
+
+// Remove "show" class from body on desktop size
+function checkWidth() {
+    if ($(window).width() < 950) {
+        $('body').removeClass('show');
+    }
+}
+
+$(window).resize(checkWidth);
