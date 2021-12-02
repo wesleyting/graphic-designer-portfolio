4b3d76cb144385d0fa03c1b4d54a6166fea875c7
Wesley Ting <wesleyting3@gmail.com>
2021-12-01 21:36:09 -0800

    Final update

---
+++ scripts/_changeLaptopImage.js
@@ -0,0 +1,9 @@
+function before(){
+    document.getElementById('laptop-image')
+    .src="../images/tokenstudio/laptop-content-2.jpg";
+}
+ 
+function after(){
+    document.getElementById('laptop-image')
+    .src="../images/tokenstudio/laptop-content.jpg";
+}
